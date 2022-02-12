c9f9dfe12861ea409c06416fc2d25f150067e9fb
Jean-Michel Fayard <jmfayard@gmail.com>
2022-02-12 18:07:26 +0100

    ./gradlew refreshVersionsCatalog + ./gradlew refreshVersionsMigrate
    ```bash
    ./gradlew refreshVersionsCatalog
    ./gradlew refreshVersionsMigrate
    ```

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -17,12 +17,12 @@ application {
 dependencies {
     implementation(project(":groovy"))
     implementation(kotlin("stdlib"))
-    implementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-    implementation("io.kotest:kotest-property:5.1.0")
-    implementation("io.kotest:kotest-extensions:5.1.0")
-    implementation("io.kotest:kotest-assertions-core:5.1.0")
-    implementation("io.ktor:ktor-velocity:1.6.7")
-    implementation("io.ktor:ktor-jackson:1.6.7")
+    implementation(libs.mockito.kotlin)
+    implementation(Testing.kotest.property)
+    implementation(libs.kotest.extensions)
+    implementation(Testing.kotest.assertions.core)
+    implementation(Ktor.features.velocity)
+    implementation(Ktor.features.jackson)
     implementation(BuildSrc.ktorMetrics)
     implementation(libs.commons.lang3)
 }
